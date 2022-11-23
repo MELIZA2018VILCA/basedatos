7d41228f320a4a9a299bdfed0468817c728c938c
meliza <201913306k@utea.edu.pe>
2022-11-23 12:51:17 -0500

    insert,get,update,delete

--- lib/db/db_admin.dart
+++ lib/db/db_admin.dart
@@ -10,7 +10,7 @@ class DBAdmin {
   static final DBAdmin db = DBAdmin._();
   DBAdmin._();
 
-  Future<Database?> chekDatabase()  async{
+  Future<Database?> chekDatabase() async {
     if (myDatabase != null) {
       return myDatabase;
     }
@@ -27,9 +27,73 @@ class DBAdmin {
       version: 1,
       onOpen: (db) {},
       onCreate: (Database dbx, int version) async {
+        //crear la tabla correspondiente
         await dbx.execute(
-            "CREATE TABLE task(id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, dni TEXT, telefono TEXT, direccion TEXT)");
+            "CREATE TABLE task(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, descripcion TEXT, status TEXT)");
       },
     );
   }
+
+  insertRawTask() async {
+    Database? db = await chekDatabase();
+    int rest = await db!.rawInsert(
+        "INSERT INTO TASK(TITLE,descripcion, status) values ('ir de compras','tenemos que ir a totus','falso')");
+    print(rest);
+  }
+
+  insertTask() async {
+    Database? db = await chekDatabase();
+    int rest = await db!.insert(
+      "TASK",
+      {
+        "title": "comprar el nuevo disco",
+        "descripcion": "nuevo disco",
+        "status": "false",
+      },
+    );
+    print(rest);
+  }
+
+  getRawTasks() async {
+    Database? db = await chekDatabase();
+    List tasks = await db!.rawQuery("SELECT * FROM Task");
+    print(tasks);
+  }
+
+  getTasks() async {
+    Database? db = await chekDatabase();
+    List tasks = await db!.query("Task");
+    print(tasks);
+  }
+
+  updateRawTasks() async {
+    Database? db = await chekDatabase();
+    db!.rawUpdate(
+        "UPDATE TASK SET title = 'ir de compras',descripcion = 'comprar comida', status = 'true' where id = 2");
+  }
+
+  updateTask() async {
+    Database? db = await chekDatabase();
+    db!.update(
+      "TASK",
+      {
+        "title": "ir al cine",
+        "descripcion": "viernes por la tarde",
+        "status": "false",
+      },
+      where: "id=2"
+    );
+  }
+  deleteRawTask()async{
+      Database? db = await chekDatabase();
+      int rest = await db!.rawDelete("DELETE FROM TASK WHERE id = 2");
+      print(rest);
+  }
+  deteleTask()async{
+    Database? db = await chekDatabase();
+    int rest = await db!.delete("TASK",where: "id=2");
+    print(rest);
+
+  }
+
 }
