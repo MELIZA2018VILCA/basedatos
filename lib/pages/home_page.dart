1f01a4993f3a795bd23541f28133e6b6fd955d3f
meliza <201913306k@utea.edu.pe>
2022-11-29 18:51:42 -0500

    video 8 finish

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:basedatos/db/db_admin.dart';
+import 'package:basedatos/models/task_model.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -18,14 +19,14 @@ class HomePage extends StatelessWidget {
         future: DBAdmin.db.getTasks(),
         builder: (BuildContext context, AsyncSnapshot  snap) {
           if (snap.hasData) {
-            List<Map<String, dynamic>> myTasks = snap.data;
+            List<taskModel> myTasks = snap.data;
             return ListView.builder(
               itemCount: myTasks.length,
               itemBuilder: (BuildContext context, int index){
                 return ListTile(
-                  title: Text(myTasks[index]["title"]),
-                  subtitle: Text(myTasks[index]["descripcion"]),
-                  trailing: Text(myTasks[index]["id"].toString()),
+                  title: Text(myTasks[index].title),
+                  subtitle: Text(myTasks[index].descripcion),
+                  trailing: Text(myTasks[index].id.toString()),
                 );
               },
             );
